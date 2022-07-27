54e7860d5690df3364b403b1c83bac9315586b29
Pablo <delatorre.pablodaniel@gmail.com>
2022-07-27 17:28:05 -0300

    archivos corregidos

--- src/routes/info.js
+++ src/routes/info.js
@@ -3,7 +3,7 @@ import process from 'process'
 
 const router = Router();
 
-const proceso = [process.cwd(),process.pid,process.version,process.memoryUsage(),process.platform]
+const proceso = {"Directorio de ejecución":process.cwd(),"ID del proceso":process.pid,"Node version":process.version,"Memoria total reservada":process.memoryUsage(),"Plataforma":process.platform,"Path de ejecución":process.execPath,"Argumentos de entrada":process.argv}
 
 router.get("/", (req, res) => {
     
